7ad9aced8c59c0ce3a9ef84c9d9af6634d180403
SathyaSrun <srundarasathya@gmail.com>
2026-07-21 15:59:36 +0700

    Configure production static engine variables

--- crm/urls.py
+++ crm/urls.py
@@ -20,5 +20,5 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('accounts.urls')),
-      path('i18n/', include('django.conf.urls.i18n')),
+       path('i18n/', include('django.conf.urls.i18n')),
 ]
